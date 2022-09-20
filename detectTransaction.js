0fd0212d36c4f5f08a47f9f84227b41b0402b293
NikitaPavlov589 <npa@takeprofittech.com>
2022-09-20 20:32:30 +0300

    first commit

--- detectTransaction.js
+++ detectTransaction.js
@@ -3,7 +3,7 @@ const client = new Client({
     user: 'postgres',
     host: '127.0.0.1',
     database: 'changemecrypto2',
-    password: 'listentherain88',
+    password: 'abc123',
     port: 5432,
   });
 
--- outgoingTransactionSender.js
+++ outgoingTransactionSender.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 const client = new Client({
     user: 'postgres',
     host: '127.0.0.1',
-    database: 'changemecrypto2',
+    database: 'abc123',
     password: 'listentherain88',
     port: 5432,
 });
